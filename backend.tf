7d3a684fa249df70372290ebbff5b4f0099f1938
Luke Lamberg <32557712+lambergluke@users.noreply.github.com>
2022-07-21 22:11:02 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name = "state"
+    resource_group_name = "tfstate"
     storage_account_name = "terraformstatelesss"
     container_name = "statey"
     key = "testcluster.tfstate"
